d44f8592a9d885ff25945eb6ed33e81eaba4ef1a
djm43@students.ptcollege.edu <>
2018-10-22 15:57:22 -0400

    this is the first commit

---
+++ src/mclaughlin/McLaughlin.java
@@ -0,0 +1,12 @@
+package mclaughlin;
+
+public class McLaughlin {
+
+    public static void main(String[] args) {
+        McLaughlin();
+    }
+    
+    public static void McLaughlin(){
+        System.out.print("Dominic J. McLaughlin");
+    }
+}
